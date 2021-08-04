b1c013c06f554defd9a32b72d4a0cdf481aa62b3
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-04 14:57:01 -0700

    finished basic instructions

---
+++ index.js
@@ -0,0 +1,60 @@
+let inputField;
+
+let message = document.querySelector("#message");
+
+console.log(message);
+
+function addMovie(event){
+  event.preventDefault()
+
+  //add movies to list
+
+  inputField = document.querySelector(`input`).value;
+
+  let movie = document.createElement(`li`);
+
+  let movieTitle = document.createElement(`span`);
+
+  movieTitle.textContent = inputField;
+
+  movie.appendChild(movieTitle);
+
+  document.querySelector(`ul`).appendChild(movie);
+
+  //clear input box
+  document.querySelector(`input`).value = "";
+  
+  //remove movies from list
+
+  // //create delete button
+  let deleteBtn = document.createElement(`button`);
+
+  deleteBtn.textContent = "X";
+
+  // //put button on movie
+  movie.appendChild(deleteBtn);
+
+  //add event listener to button to run deleteMovie when clicked
+  deleteBtn.addEventListener(`click`, deleteMovie);
+
+    //add event listener to list item to run crossOffMovie when clicked
+  movieTitle.addEventListener(`click`, crossOffMovie);
+}
+
+document.querySelector("form").addEventListener('submit', addMovie);
+
+function deleteMovie(event){
+  event.target.parentNode.remove();
+  message.textContent = "Movie deleted!";
+}
+
+function crossOffMovie(event){
+  event.target.classList.toggle("checked");
+
+  if(event.target.classList.contains("checked")){
+    message.textContent = "Movie watched!";
+  }
+  else{
+    message.textContent = "Movie added back!";
+  }
+}
